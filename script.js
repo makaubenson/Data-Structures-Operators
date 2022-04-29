834000e045c54af45a8976edd85289dc8cebbda0
Benson <bensonmakau2000@gmail.com>
2022-04-29 21:54:18 +0300

    AND Assignment Operator (&&)

--- script.js
+++ script.js
@@ -82,6 +82,10 @@ const rest2 = {
 rest1.numGuests ??= 10;
 rest2.numGuests ??= 10;
 
+//AND Assignment Operator (&&)
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
 console.log(rest1);
 console.log(rest2);
 /*
