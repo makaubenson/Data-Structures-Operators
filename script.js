74b3a6977ec04a0d9350c9895168d25bcc73c64c
Benson <bensonmakau2000@gmail.com>
2022-05-06 13:50:58 +0300

    solved step1 of my coding challenge

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -78,7 +78,7 @@ for (const x of entries) {
 for (const [day, { open, close }] of entries) {
   console.log(`on ${day}, we open at ${open} and close at ${close}`);
 }
-/*
+
 //Without Optional Chaining
 if (restaurant.openingHours && restaurant.openingHours.mon) {
   console.log(restaurant.openingHours.mon.open);
@@ -452,3 +452,53 @@ printGoals(...game.scored);
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2  is more likely to win');
 */
+//Coding Challenge
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// Loop Over game.scored and print each player to the console together with the number of goals scored
+const scorers = Object.entries(game.scored);
+console.log(scorers);
+for (let [goal, player] of scorers) {
+  goal = parseInt(goal);
+  goal = goal + 1;
+  console.log(`Goal ${goal}  : ${player}`);
+}
